7b5ea08814e5313ea57f21bd53134f4c25195308
paulaarnol <paulalihuearnolacuna@gmail.com>
2026-06-03 13:54:44 -0300

    Adaptación para celular.

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
+
 const header = document.getElementById("header");
 
-window.addEventListener("scroll", function() {
-  if (window.scrollY > 50) {
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 50){
     header.classList.add("scrolled");
-  } else {
+  }else{
     header.classList.remove("scrolled");
   }
-});
 
-window.addEventListener("DOMContentLoaded", function() {
-  const header = document.getElementById("header");
-
-  window.addEventListener("scroll", function() {
-    if (window.scrollY > 50) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
 });
 
 const container = document.querySelector(".productos-container");
 
-document.querySelector(".next").onclick = () => {
-  container.scrollLeft += 300;
-};
+const next = document.querySelector(".next");
 
-document.querySelector(".prev").onclick = () => {
-  container.scrollLeft -= 300;
-};
+const prev = document.querySelector(".prev");
+
+function getScrollAmount(){
+
+  if(window.innerWidth < 768){
+    return 250;
+  }
+
+  return 350;
+}
+
+next.addEventListener("click", () => {
+
+  container.scrollLeft += getScrollAmount();
+
+});
+
+prev.addEventListener("click", () => {
+
+  container.scrollLeft -= getScrollAmount();
+
+});
